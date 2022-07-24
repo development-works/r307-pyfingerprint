b5f86ceeeef9155d581a1564835f7990a0a60eb8
Diksha Mehta <dikshamehta4214t@gmail.com>
2022-07-24 17:29:46 +0530

    Download Char Buffer Function

--- r307_fingerprint.py
+++ r307_fingerprint.py
@@ -15,6 +15,7 @@ IC_GENERATE_IMAGE = bytes.fromhex('01')
 IC_DOWNLOAD_IMAGE = bytes.fromhex('0a')
 IC_GENERATE_CHARACTERISTICS = bytes.fromhex('02')
 IC_GENERATE_TEMPLATE = bytes.fromhex('05')
+IC_DOWNLOAD_CHAR_BUFFER = bytes.fromhex('08')
 
 CC_SUCCESS = bytes.fromhex('00')
 CC_ERROR = bytes.fromhex('01')
@@ -26,9 +27,11 @@ CC_DISORDERED_FINGERPRINT = bytes.fromhex('06')
 CC_VERY_SMALL_FINGERPRINT = bytes.fromhex('07')
 CC_INVALID_PRIMARY_IMAGE = bytes.fromhex('15')
 CC_CHAR_MISMATCH = bytes.fromhex('0a')
+CC_TEMPLATE_DWNLD_ERR = bytes.fromhex('0d')
 
 CHAR_BUFFER_1 = bytes.fromhex('01')
 CHAR_BUFFER_2 = bytes.fromhex('02')
+
 
 # download char buffer
 # download image
@@ -52,7 +55,7 @@ class Sensor:
 
         self.send_packet(PID_COMMAND, IC_VERIFY_PASSWORD + self._password)
 
-        pid_rcv, content_rcv = self.recieve_packet()
+        pid_rcv, content_rcv = self.receive_packet()
 
         cc = content_rcv
 
@@ -69,9 +72,8 @@ class Sensor:
         time.sleep(2)
         self.send_packet(PID_COMMAND, IC_GENERATE_IMAGE)
 
-        pid_rcv, content_rcv = self.recieve_packet()
+        pid_rcv, content_rcv = self.receive_packet()
         cc = content_rcv
-
 
         if cc == CC_SUCCESS:
             print("Finger Collection Success")
@@ -86,7 +88,7 @@ class Sensor:
 
     def download_image(self):
         self.send_packet(PID_COMMAND, IC_DOWNLOAD_IMAGE)
-        pid_rcv, content_rcv = self.recieve_packet()
+        pid_rcv, content_rcv = self.receive_packet()
 
         cc = content_rcv
 
@@ -104,24 +106,23 @@ class Sensor:
 
         with open('temp/img.jpg', 'ab') as file:
             while True:
-                pid_rcv, content_rcv = self.recieve_packet()
+                pid_rcv, content_rcv = self.receive_packet()
                 image_rcv = image_rcv + content_rcv
 
                 if pid_rcv == PID_EOD:
                     print("End of Data reached")
                     break
 
-
-        #TODO: Save the File in image form
+        # TODO: Save the File in image form
 
         # finger_img = Image.frombytes("L", (256, 288), image_rcv)
         # finger_img.save("./temp/FingerPrintImage.jpg")
 
     def generate_charfile_image(self, buffer_id):
         self.send_packet(PID_COMMAND,
-                         IC_GENERATE_CHARACTERISTICS+buffer_id)
+                         IC_GENERATE_CHARACTERISTICS + buffer_id)
 
-        pid_rcv, content_rcv = self.recieve_packet()
+        pid_rcv, content_rcv = self.receive_packet()
         cc = content_rcv
 
         if cc == CC_SUCCESS:
@@ -134,7 +135,8 @@ class Sensor:
                             "over-disorderly fingerprint image")
         elif cc == CC_VERY_SMALL_FINGERPRINT:
             raise Exception("fail to generate character file due to lackness "
-                            "of character point or over-smallness of fingerprint image")
+                            "of character point or over-smallness of "
+                            "fingerprint image")
         elif cc == CC_INVALID_PRIMARY_IMAGE:
             raise Exception("fail to generate the image for the lackness of "
                             "valid primary image")
@@ -145,7 +147,7 @@ class Sensor:
         self.send_packet(PID_COMMAND,
                          IC_GENERATE_TEMPLATE)
 
-        pid_rcv, content_rcv = self.recieve_packet()
+        pid_rcv, content_rcv = self.receive_packet()
         cc = content_rcv
 
         if cc == CC_SUCCESS:
@@ -159,6 +161,33 @@ class Sensor:
         else:
             raise Exception("Unrecognised confirmation code")
 
+    def download_char_buffer(self, buffer_id):
+        self.send_packet(PID_COMMAND, IC_DOWNLOAD_CHAR_BUFFER + buffer_id)
+        pid_rcv, content_rcv = self.receive_packet()
+
+        cc = content_rcv
+
+        if cc == CC_SUCCESS:
+            print("Downloading the fingerprint image")
+        elif cc == CC_ERROR:
+            raise Exception("error when receiving package for downloading "
+                            "image")
+        elif cc == CC_TEMPLATE_DWNLD_ERR:
+            raise Exception("Error when downloading template")
+        else:
+            raise Exception("Unrecognised confirmation code")
+
+        char_rcv = bytes()
+
+        while True:
+            pid_rcv, content_rcv = self.receive_packet()
+            char_rcv = char_rcv + content_rcv
+
+            if pid_rcv == PID_EOD:
+                print("End of Data reached")
+                break
+
+        print(char_rcv)
 
     def checksum(self, pid, package_len, content):
         checksum = int.from_bytes(pid, byteorder='big') + package_len
@@ -189,11 +218,11 @@ class Sensor:
         self._serial.write(HEADER)
         self._serial.write(self._address)
         self._serial.write(pid)
-        self._serial.write(package_len.to_bytes(2,byteorder='big'))
+        self._serial.write(package_len.to_bytes(2, byteorder='big'))
         self._serial.write(content)
         self._serial.write(checksum)
 
-    def recieve_packet(self):
+    def receive_packet(self):
         header_rcv = self._serial.read(2)
         if header_rcv != HEADER:
             raise Exception("Acknowledgment Header invalid")
@@ -209,7 +238,7 @@ class Sensor:
         len_rcv = self._serial.read(2)
         len_rcv = int.from_bytes(len_rcv, byteorder='big')
 
-        content_rcv = self._serial.read(len_rcv-2)
+        content_rcv = self._serial.read(len_rcv - 2)
 
         checksum_rcv = self._serial.read(2)
         if checksum_rcv != self.checksum(pid_rcv, len_rcv, content_rcv):
@@ -218,10 +247,10 @@ class Sensor:
         return pid_rcv, content_rcv
 
 
-
 sensor = Sensor('/dev/ttyUSB0', 57600)
 # sensor.generate_image()
-#sensor.download_image()
+# sensor.download_image()
 # sensor.generate_charfile_image(CHAR_BUFFER_1)
 # sensor.generate_charfile_image(CHAR_BUFFER_2)
-sensor.generate_template()
+# sensor.generate_template()
+sensor.download_char_buffer(CHAR_BUFFER_1)
